1da4483073d478f1a9682045edaae39748be5d0b
eemmresen <eemmresen@gmail.com>
2020-01-06 10:57:34 +0300

    Add files via upload

---
+++ proje/static/script.js
@@ -0,0 +1,50 @@
+const e = new Audio('static/sounds/8397__speedy__clean-e-str-pluck.wav')
+
+function eSound() {
+    e.currentTime = 0;
+    e.play();
+}
+
+const a = new Audio('static/sounds/8383__speedy__clean-a-str-pluck.wav')
+
+function aSound() {
+    a.currentTime = 0;
+    a.play();
+}
+
+const d = new Audio('static/sounds/8389__speedy__clean-d-str-pluck.wav')
+
+function dSound() {
+    d.currentTime = 0;
+    d.play();
+}
+
+
+const g = new Audio('static/sounds/8403__speedy__clean-g-str-pluck.wav')
+
+function gSound() {
+    g.currentTime = 0;
+    g.play();
+}
+
+const b = new Audio('static/sounds/8386__speedy__clean-b-str-pluck.wav')
+
+function bSound() {
+    b.currentTime = 0;
+    b.play();
+}
+
+const e1 = new Audio('static/sounds/8394__speedy__clean-e1st-str-pluck.wav')
+
+function e1Sound() {
+    e1.currentTime = 0;
+    e1.play();
+}
+
+
+e.volume = 0.2
+a.volume = 0.2
+d.volume = 0.2
+g.volume = 0.2
+b.volume = 0.2
+e1.volume = 0.2
